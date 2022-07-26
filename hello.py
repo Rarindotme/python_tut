92f16134546759ba4a336dfcd8a9e3bbd5394687
Rarindotme <rarin@rarin.me>
2022-07-26 15:23:08 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -42,3 +42,14 @@ elif not(is_male) and is_tall:
   print("Tall Female")
 else:
   print("Not Male Not Tall")   
+
+#comparison
+def max_num(num1, num2, num3):
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  else:
+    return num3
+
+print(max_num(3,4,5))
