a385a5985cb745e7913ec5ad68ca8bb41ab3a5aa
Rarindotme <rarin@rarin.me>
2022-07-27 11:01:45 +0300

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,4 +1,7 @@
 # Dictionary 
+from calendar import month
+
+
 month_conversions = {
   "Jan": "January",
   "feb": "February",
@@ -6,7 +9,7 @@ month_conversions = {
   "Apr": "April",
   "May": "May",
   "Jun": "June",
-  "Jul": "July",
+  7: "July",
   "Aug": "August",
   "Sept": "September",
   "Oct": "October",
@@ -14,3 +17,12 @@ month_conversions = {
   "Dec": "December"
 }
 print(month_conversions["Oct"])
+print(month_conversions.get("Dec"))
+print(month_conversions.get("luv", "Not a valid Key"))
+
+print(month_conversions[7])
+
+i = 1
+while i <= 10:
+  print(i)
+  i += 1
